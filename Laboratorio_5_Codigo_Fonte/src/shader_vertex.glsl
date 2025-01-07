a425e3188b7d793139390a83d27b36d717fc1d5e
Lucca Claus <luccaclaus@hotmail.com>
2025-01-07 01:10:21 -0300

    added gouraud shading with blinn-phong lighting

--- Laboratorio_5_Codigo_Fonte/src/shader_vertex.glsl
+++ Laboratorio_5_Codigo_Fonte/src/shader_vertex.glsl
@@ -19,6 +19,14 @@ out vec4 position_world;
 out vec4 position_model;
 out vec4 normal;
 out vec2 texcoords;
+out vec4 vertexcolor;     // Cor interpolada para o fragment shader
+
+// Variáveis para acesso das imagens de textura
+uniform sampler2D TextureImage0;
+
+uniform int object_id;
+#define ISLAND 0
+
 
 void main()
 {
@@ -63,5 +71,86 @@ void main()
 
     // Coordenadas de textura obtidas do arquivo OBJ (se existirem!)
     texcoords = texture_coefficients;
+
+    // Cálculo do Gouraud Shading
+    // Usamos a mesma forma de calcular lighting no fragment shader só que no vertex shader, dessa forma passando já o valor correto das cores para o fragment shader
+    if (object_id == ISLAND) {
+
+        // ----- Inicialização padrão para cálculos de modelos de iluminação
+
+        // Obtemos a posição da câmera utilizando a inversa da matriz que define o
+        // sistema de coordenadas da câmera.
+        vec4 origin = vec4(0.0, 0.0, 0.0, 1.0);
+        vec4 camera_position = inverse(view) * origin;
+
+        // O fragmento atual é coberto por um ponto que percente à superfície de um
+        // dos objetos virtuais da cena. Este ponto, p, possui uma posição no
+        // sistema de coordenadas global (World coordinates). Esta posição é obtida
+        // através da interpolação, feita pelo rasterizador, da posição de cada
+        // vértice.
+        vec4 p = position_world;
+
+        // Normal do fragmento atual, interpolada pelo rasterizador a partir das
+        // normais de cada vértice.
+        vec4 n = normalize(normal);
+
+        // Vetor que define o sentido da fonte de luz em relação ao ponto atual.
+        vec4 l = normalize(vec4(1.0,1.0,0.0,0.0));
+
+        // Vetor que define o sentido da câmera em relação ao ponto atual.
+        vec4 v = normalize(camera_position - p);
+
+        // Vetor que define o sentido da reflexão especular ideal.
+        vec4 r = reflect(-l, n); 
+
+        // Vetor h utilizado no cálculo do modelo de iluminação de Blinn-Phong
+        vec4 h = normalize(v + l);
+
+        // Parâmetros que definem as propriedades espectrais da superfície
+        vec3 Kd; // Refletância difusa
+        vec3 Ks; // Refletância especular
+        vec3 Ka; // Refletância ambiente
+        float q; // Expoente especular para o modelo de iluminação de Phong
+
+        // Coordenadas de textura U e V
+        float U = 0.0;
+        float V = 0.0;
+
+        // --------------
+
+        U = texcoords.x;
+        V = texcoords.y;
+
+        // Obtemos a refletância difusa a partir da leitura da imagem TextureImage0
+        vec3 Kd0 = texture(TextureImage0, vec2(U,V)).rgb;
+
+        Ks = vec3(0.5,0.5,0.5);
+        Ka = Kd0 / 2.0;
+        q = 70.0;
+
+        // Espectro da fonte de iluminação
+        vec3 I = vec3(1.0,1.0,1.0); 
+
+        // Espectro da luz ambiente
+        vec3 Ia = vec3(0.02,0.02,0.02); 
+
+        // Equação de Iluminação
+        float lambert = max(0,dot(n,l));
+
+        // Iluminação ambiente
+        vec3 ambient_term = Ka * Ia; 
+
+        // Termo especular utilizando o modelo de iluminação de Blinn-Phong
+        vec3 blinn_phong_specular_term = Ks * I * pow(max(0, dot(n,h)), q);
+
+        // Uso do modelo de iluminação de Blinn-Phong
+        vertexcolor.rgb = (Kd0 * I * (lambert + 0.05)) + ambient_term + blinn_phong_specular_term;
+
+        vertexcolor.a = 1;
+
+        // Cor final com correção gamma, considerando monitor sRGB.
+        // Veja https://en.wikipedia.org/w/index.php?title=Gamma_correction&oldid=751281772#Windows.2C_Mac.2C_sRGB_and_TV.2Fvideo_standard_gammas
+        vertexcolor.rgb = pow(vertexcolor.rgb, vec3(1.0,1.0,1.0)/1.2);
+    }
 }
 
